8b210a12e7e2f4ba4d91b9e809cd92f129a8738c
Nurul Amira <mirazn176@gmail.com>
2023-01-14 15:49:29 +0800

    Update DependentPage.java

--- app/src/main/java/com/example/mytrace/Application/Dependent/DependentPage.java
+++ app/src/main/java/com/example/mytrace/Application/Dependent/DependentPage.java
@@ -9,6 +9,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -20,6 +21,7 @@ import com.example.mytrace.R;
 public class DependentPage extends AppCompatActivity {
 
     ImageView edit_depen;
+    ImageView imageView2;
     Button Add_new_Depen;
 
     @Override
@@ -28,13 +30,45 @@ public class DependentPage extends AppCompatActivity {
         setContentView(R.layout.dependent_page);
 
         edit_depen =(ImageView) findViewById(R.id.edit_depen);
+        imageView2 =(ImageView) findViewById(R.id.imageView2);
         Add_new_Depen =(Button) findViewById(R.id.Add_new_Depen);
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
 
         // this Listener to get to Dependet Details page
         edit_depen.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(DependentPage.this, Dependent_Details.class));
+            }
+        });
+
+        imageView2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                builder.setMessage("Do you want to delete this dependent ?")
+                        .setCancelable(false)
+                        .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                finish();
+                                Toast.makeText(getApplicationContext(),"Dependent has been deleted",
+                                        Toast.LENGTH_SHORT).show();
+                                startActivity(new Intent(DependentPage.this, DependentPage.class));
+                            }
+                        })
+                        .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                //  Action for 'NO' Button
+                                dialog.cancel();
+                                Toast.makeText(getApplicationContext(),"Deleted Cancel",
+                                        Toast.LENGTH_SHORT).show();
+                            }
+                        });
+                //Creating dialog box
+                AlertDialog alert = builder.create();
+                //Setting the title manually
+                alert.setTitle("Delete Dependent");
+                alert.show();
+
             }
         });
 
