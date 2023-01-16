f54aa592061c645451f40034d0cc9f03826abb77
Nurul Amira <mirazn176@gmail.com>
2023-01-16 08:24:24 +0800

    update

--- app/src/main/java/com/example/mytrace/Application/Dependent/Add_Dependent.java
+++ app/src/main/java/com/example/mytrace/Application/Dependent/Add_Dependent.java
@@ -19,6 +19,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.mytrace.R;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
@@ -31,15 +33,13 @@ import com.google.firebase.database.ValueEventListener;
 
 public class Add_Dependent extends AppCompatActivity {
 
-    private EditText name, Relation, Gender;
+    private EditText name, Relation, Gender,CurrAddress, IC, PNumber, State,age;
     Button Add_Depen_button;
     Spinner citizen;
-    //FirebaseFirestore firestore;
-    //FirebaseDatabase database = FirebaseDatabase.getInstance();
-   // DatabaseReference myRef = database.getReference("message");
+
     FirebaseDatabase firebaseDatabase;
     DatabaseReference databaseReference;
-    Add_dependent_Db addDb;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -48,7 +48,7 @@ public class Add_Dependent extends AppCompatActivity {
 
         firebaseDatabase = FirebaseDatabase.getInstance();
 
-        //databaseReference = firebaseDatabase.getReference("Add_dependent_Db");
+
 
 
 
@@ -56,12 +56,17 @@ public class Add_Dependent extends AppCompatActivity {
         Relation=(EditText) findViewById(R.id.Relation) ;
         name=(EditText) findViewById(R.id.name) ;
         Gender=(EditText) findViewById(R.id.Gender) ;
+        CurrAddress=(EditText) findViewById(R.id.CurrAddress) ;
+        IC=(EditText) findViewById(R.id.IC) ;
+        PNumber=(EditText) findViewById(R.id.PNumber) ;
+        State=(EditText) findViewById(R.id.State) ;
+        age=(EditText) findViewById(R.id.age) ;
         Add_Depen_button=(Button)findViewById(R.id.Add_Depen_button) ;
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
 
         firebaseDatabase = FirebaseDatabase.getInstance();
         databaseReference = firebaseDatabase.getReference("Add_dependent_Db");
-         addDb = new Add_dependent_Db();
+
         // this Listener to get to Dependent Details page
         Add_Depen_button.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -69,90 +74,87 @@ public class Add_Dependent extends AppCompatActivity {
 
                 String relation = Relation.getText().toString();
                 String gender = Gender.getText().toString();
+                String currAdd = CurrAddress.getText().toString();
+                String ic = IC.getText().toString();
+                String Age = age.getText().toString();
+                String num = PNumber.getText().toString();
+                String state = State.getText().toString();
                 String Name = name.getText().toString();
-
+                String Citizen = citizen.getSelectedItem().toString();
                 if (TextUtils.isEmpty(relation) && TextUtils.isEmpty(gender) && TextUtils.isEmpty(Name)) {
                     // if the text fields are empty
                     // then show the below message.
-                    Toast.makeText(Add_Dependent.this, "Please add some data.", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(Add_Dependent.this, "Please Add Some Data", Toast.LENGTH_SHORT).show();
                 } else {
                     // else call the method to add
                     // data to our database.
-                    addDatatoFirebase(Name, gender, relation);
+                    builder.setMessage("Do you want to add this dependent ?")
+                            .setCancelable(false)
+                            .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int id) {
+                                    finish();
+//                                    Toast.makeText(getApplicationContext(),"Dependent has been added",
+//                                            Toast.LENGTH_SHORT).show();
+                                    addDatatoFirebase(new Add_dependent_Db(Name,gender,relation,Citizen,currAdd,ic,Age, num,state ));
+                                    //startActivity(new Intent(Add_Dependent.this, DependentPage.class));
+                                }
+                            })
+                            .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int id) {
+                                    //  Action for 'NO' Button
+                                    dialog.cancel();
+                                    Toast.makeText(getApplicationContext(),"Add New Dependent Cancel",
+                                            Toast.LENGTH_SHORT).show();
+                                }
+                            });
+                    //Creating dialog box
+                    AlertDialog alert = builder.create();
+                    //Setting the title manually
+                    alert.setTitle("Add Dependent");
+                    alert.show();
+//                    AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(Add_Dependent.this);
+//                    alertDialogBuilder.setTitle("Confirmation");
+//
+//                    alertDialogBuilder.setMessage("Are You Sure Want to Add New Dependent")
+//                            .setCancelable(false)
+//                            .setPositiveButton("yes", new DialogInterface.OnClickListener() {
+//                                @Override
+//                                public void onClick(DialogInterface dialog, int which) {
+//
+//
+//                                }
+//                            }).setNegativeButton("no", new DialogInterface.OnClickListener(){
+//
+//                                @Override
+//                                public void onClick(DialogInterface dialog, int which) {
+//                                    dialog.cancel();
+//                                }
+//                            });
+                    //startActivity(new Intent(Add_Dependent.this, DependentPage.class));
+
                 }
 
 
-                builder.setMessage("Do you want to add this dependent ?")
-                        .setCancelable(false)
-                        .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
-                            public void onClick(DialogInterface dialog, int id) {
-                                finish();
-                                Toast.makeText(getApplicationContext(),"Dependent has been added",
-                                        Toast.LENGTH_SHORT).show();
-                                startActivity(new Intent(Add_Dependent.this, DependentPage.class));
-                            }
-                        })
-                        .setNegativeButton("No", new DialogInterface.OnClickListener() {
-                            public void onClick(DialogInterface dialog, int id) {
-                                //  Action for 'NO' Button
-                                dialog.cancel();
-                                Toast.makeText(getApplicationContext(),"Add New Dependent Cancel",
-                                        Toast.LENGTH_SHORT).show();
-                            }
-                        });
-                //Creating dialog box
-                AlertDialog alert = builder.create();
-                //Setting the title manually
-                alert.setTitle("Add Dependent");
-                alert.show();
-                AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(Add_Dependent.this);
-                alertDialogBuilder.setTitle("Confirmation");
 
-                alertDialogBuilder.setMessage("Are You Sure Want to Add New Dependent")
-                                .setCancelable(false)
-                                        .setPositiveButton("yes", new DialogInterface.OnClickListener() {
-                                            @Override
-                                            public void onClick(DialogInterface dialog, int which) {
-                                                startActivity(new Intent(Add_Dependent.this, DependentPage.class));
-                                            }
-                                        }).setNegativeButton("no", new DialogInterface.OnClickListener(){
 
-                            @Override
-                            public void onClick(DialogInterface dialog, int which) {
-                                dialog.cancel();
-                            }
-                        });
-                //startActivity(new Intent(Add_Dependent.this, DependentPage.class));
-
-            }
+           }
         });
     }
-    private void addDatatoFirebase(String name, String gender, String relation) {
-        // below 3 lines of code is used to set
-        // data in our object class.
-        addDb.setName(name);
-        addDb.setGender(gender);
-        addDb.setRelation(relation);
-
-        // we are use add value event listener method
-        // which is called with database reference.
-        databaseReference.addValueEventListener(new ValueEventListener() {
+    private void addDatatoFirebase(Add_dependent_Db addDb) {
+        DatabaseReference dependent = databaseReference.child("Dependent").push();
+        dependent.setValue(addDb).addOnSuccessListener(new OnSuccessListener<Void>() {
             @Override
-            public void onDataChange(@NonNull DataSnapshot snapshot) {
-                // inside the method of on Data change we are setting
-                // our object class to our database reference.
-                // data base reference will sends data to firebase.
-                databaseReference.setValue(addDb);
-
-                // after adding this data we are showing toast message.
-                Toast.makeText(Add_Dependent.this, "data added", Toast.LENGTH_SHORT).show();
+            public void onSuccess(Void unused) {
+                Toast.makeText(getApplicationContext(),"Success insert",
+                        Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(Add_Dependent.this, DependentPage.class));
             }
 
+        }).addOnFailureListener(new OnFailureListener() {
             @Override
-            public void onCancelled(@NonNull DatabaseError error) {
-                // if the data is not added or it is cancelled then
-                // we are displaying a failure toast message.
-                Toast.makeText(Add_Dependent.this, "Fail to add data " + error, Toast.LENGTH_SHORT).show();
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(getApplicationContext(),"Failed insert",
+                        Toast.LENGTH_SHORT).show();
             }
         });
     }
--- app/src/main/java/com/example/mytrace/Application/Dependent/Add_dependent_Db.java
+++ app/src/main/java/com/example/mytrace/Application/Dependent/Add_dependent_Db.java
@@ -3,15 +3,15 @@ package com.example.mytrace.Application.Dependent;
 public class Add_dependent_Db {
     // string variable for
     // storing employee name.
-  //  private String citizen;
+    private String citizen;
     private String Relation;
     private String Gender;
-//    private String CurrAddress;
- //   private String IC;
-  //  private String age;
+   private String CurrAddress;
+   private String IC;
+    private String age;
     private String name;
-//    private String PNumber;
-//    private String State;
+    private String PNumber;
+   private String State;
 
 
 
@@ -22,30 +22,102 @@ public class Add_dependent_Db {
 
     }
 
+    public Add_dependent_Db(String name, String Gender, String Relation, String citizen,String CurrAddress,String IC,String age,String PNumber,String State) {
+        this.name = name;
+        this.Gender = Gender;
+        this.Relation = Relation;
+        this.citizen = citizen;
+        this.age = age;
+        this.CurrAddress = CurrAddress;
+        this.PNumber = PNumber;
+        this.State = State;
+        this.IC = IC;
+    }
+
     // created getter and setter methods
     // for all our variables.
     public String getName() {
+
         return name;
     }
 
     public void setName(String name) {
+
         this.name = name;
     }
 
     public String getRelation() {
+
         return Relation;
     }
 
     public void setRelation(String Relation) {
+
         this.Relation = Relation;
     }
 
     public String getGenders() {
+
         return Gender;
     }
 
     public void setGender(String Gender) {
+
         this.Gender = Gender;
     }
+    public String getCurrAddress() {
+
+        return CurrAddress;
+    }
+
+    public void setCurrAddress(String CurrAddress) {
+
+        this.CurrAddress = CurrAddress;
+    }
+    public String getIC() {
+
+        return IC;
+    }
+
+    public void setIC(String IC) {
+
+        this.IC = IC;
+    }
+    public String getage() {
+
+        return age;
+    }
+
+    public void setage(String age) {
+
+        this.age = age;
+    }
+    public String getPNumber() {
+
+        return PNumber;
+    }
+
+    public void setPNumber(String PNumber) {
+
+        this.PNumber = PNumber;
+    }
+    public String getCitizen() {
+
+        return citizen;
+    }
+    public void setCitizen(String citizen) {
+
+        this.citizen = citizen;
+    }
+    public void setState(String State) {
+
+        this.State = State;
+    }
+    public String getState() {
+
+        return State;
+    }
+
+
 }
 
