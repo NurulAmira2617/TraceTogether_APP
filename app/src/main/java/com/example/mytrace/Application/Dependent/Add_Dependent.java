40c567c085355e5c91e5e09bc7ae289c0e291252
Nurul Amira <mirazn176@gmail.com>
2023-01-27 21:24:43 +0800

    27/1

--- app/src/main/java/com/example/mytrace/Application/Dependent/Add_Dependent.java
+++ app/src/main/java/com/example/mytrace/Application/Dependent/Add_Dependent.java
@@ -9,10 +9,10 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ImageView;
-import android.widget.Spinner;
-import android.widget.Toast;
+//import android.widget.EditText;
+//import android.widget.ImageView;
+//import android.widget.Spinner;
+//import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
